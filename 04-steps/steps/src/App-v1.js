f146fcdb13bcdbd31ae8913621ecabfe5b8311ac
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-07-28 10:50:07 -0700

    91

--- 04-steps/steps/src/App-v1.js
+++ 04-steps/steps/src/App-v1.js
@@ -30,41 +30,22 @@ export default function App() {
           <p className='message'>
             Step {step}: {messages[step - 1]}
           </p>
-
           <div className='buttons'>
-            <Button
-              textColor={'#fff'}
-              bgColor='#7950F2'
+            <button
+              style={{ backgroundColor: '#7950F2', color: '#fff' }}
               onClick={handlePrevious}
             >
-              <span>{'<--'}</span> Previous
-            </Button>
-            <Button textColor={'#fff'} bgColor='#7950F2' onClick={handleNext}>
-              Next <span>{'-->'}</span>
-            </Button>
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: '#7950F2', color: '#fff' }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </>
-  );
-}
-
-function StepMessage(step, children) {
-  return (
-    <div className='message'>
-      <h3>Step {step}</h3>
-      {children}
-    </div>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
