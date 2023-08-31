ed3ee95fb48b5ff6145c3b71516d8039c4f01287
Isaac Perk <izek@rill.local>
2023-08-30 20:40:57 -0700

    274

--- 15-redux-intro/redux-intro/src/features/customers/customerSlice.js
+++ 15-redux-intro/redux-intro/src/features/customers/customerSlice.js
@@ -36,5 +36,5 @@ export function createCustomer(fullName, nationalID) {
 }
 
 export function updateName(fullName) {
-  return { type: 'account/updateName', payload: fullName };
+  return { type: 'customer/updateName', payload: fullName };
 }
